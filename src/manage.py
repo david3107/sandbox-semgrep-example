58d0fca6150446459770822884296e8d85178d92
Davide Cioccia <davide.cioccia@live.it>
2025-04-07 17:00:39 +0200

    Update manage.py

--- src/manage.py
+++ src/manage.py
@@ -4,9 +4,18 @@ import os
 import sys
 
 
+
+
+
+
+
+
+
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
     try:
+        
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
